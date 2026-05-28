ee5b74e3129c905ee89c8b943ad80493a8c11935
matheut <matheus.teixeira@tjpe.jus.br>
2026-05-28 10:34:59 -0300

    Feat: implementação parcial de RAG

--- estimator-prototype/src/main/java/com/tjpe/jus/br/estimator_prototype/controller/EstimationController.java
+++ estimator-prototype/src/main/java/com/tjpe/jus/br/estimator_prototype/controller/EstimationController.java
@@ -5,11 +5,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.ai.chat.client.AdvisorParams;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.ollama.api.OllamaChatOptions;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.tjpe.jus.br.estimator_prototype.dto.EstimativeResponse;
+import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
 
 @RestController
 @RequestMapping("api/v1/estimator")
@@ -18,8 +21,10 @@ public class EstimationController {
     private static final Logger logger = LoggerFactory.getLogger(EstimationController.class);
     
     private final ChatClient chatClient;
+    private final VectorStore vectorStore;
 
-    public EstimationController(ChatClient.Builder builder) {
+    public EstimationController(ChatClient.Builder builder, VectorStore vectorStore) {
+        this.vectorStore = vectorStore;
         this.chatClient = builder
             .defaultSystem("""
                 You are an expert Agile Scrum Master and Senior Software Engineer. Your job is to estimate the development time (in hours) for a technical task based on its description.
@@ -37,10 +42,20 @@ public class EstimationController {
 
         String basicPrompt = "Estimate the hours needed to build this task: " + description;
 
+        SearchRequest searchRequest = SearchRequest.builder()
+                .topK(3)
+                .similarityThreshold(0.7)
+                .build();
+
+        var qaAdvisor = QuestionAnswerAdvisor.builder(this.vectorStore)
+            .searchRequest(searchRequest)
+            .build();
+
         return this.chatClient.prompt()
         .options(OllamaChatOptions.builder()
                 .temperature(0.0)
                 .topP(0.1))
+        .advisors(qaAdvisor)
         .advisors(AdvisorParams.ENABLE_NATIVE_STRUCTURED_OUTPUT)
         .user(basicPrompt)
         .call()
